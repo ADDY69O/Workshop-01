e1ee4a64e01f4736e67449b0863279aede8ed3c8
Aditya <kohsa9211420@gmail.com>
2024-01-19 10:29:08 +0530

    added new task within newFeatures

--- index.js
+++ index.js
@@ -9,11 +9,26 @@ console.log("within New Features  BRanch")
 function newFeatures(){
 
     console.log("all the new features")
+    function newTask (){
+        console.log("added new task")
+    }
+
+}
+
+
+//this are the devloepement data
+
+console.log("within Devlopment  BRanch")
+function Devlopement(){
+
+    console.log("all the details of devlopment")
 
 }
 
 
 
+
+//main master funtion
 function master(){
 
     console.log("within the master branch")
