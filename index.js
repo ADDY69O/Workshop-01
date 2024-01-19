0de7473122526dbe91831998a1168ea64c71e284
Aditya <kohsa9211420@gmail.com>
2024-01-19 10:24:16 +0530

    updated master branch

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("within Devlopment  BRanch")
+
+console.log("master branch")
+
+
+
+
+//this are the new features data
+console.log("within New Features  BRanch")
+function newFeatures(){
+
+    console.log("all the new features")
+
+}
+
+
+
+function master(){
+
+    console.log("within the master branch")
+}
